c308fbe8012f56c8032c9671517190b618133c77
Fy Rakotondrabe <rakotondrabefy10@gmail.com>
2024-06-19 13:18:37 +0300

    remove video tuto

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,19 +1,14 @@
 import InfoIcon from '@mui/icons-material/InfoOutlined';
 import { Box, Tooltip, Typography } from "@mui/material";
 import MarkdownPreviewer from '@uiw/react-markdown-preview';
-import VideoPlayer from 'react-player';
 import { Fields, Nodes } from "../constants/constants";
 import { Element } from "../model";
 import FormElementItem from "./Element";
-
-import pageVideo from '../assets/page.mov';
-import nodeVideo from '../assets/node.mov';
 
 interface SidebarElement {
   title: string;
   description: string;
   elements: Element[];
-  tutorialVideo: string;
 }
 
 const sidebarElements: SidebarElement[] = [
@@ -21,13 +16,11 @@ const sidebarElements: SidebarElement[] = [
     title: 'Nodes',
     description: 'Nodes are the building blocks of your form. They can be dragged and dropped inside the dropzone to compose the form and establish the relationship between entities, forms, and pages. Here are some key points to remember:\n\n- **Entity**: An entity can only be connected to one form.\n- **Form**: A form can be connected to one or many pages.\n- **Entity-Page Connection**: An entity cannot be directly connected to a page.',
     elements: Nodes,
-    tutorialVideo: nodeVideo,
   },
   {
     title: 'Fields',
     description: 'Fields are the elements that can be added to your pages. They can only be dropped inside a page.',
     elements: Fields,
-    tutorialVideo: pageVideo,
   },
 ]
 
@@ -43,7 +36,6 @@ const renderSidebarElements = (element: SidebarElement, index: number) => (
         placement='right'
         title={
           <Box sx={{ padding: 1 }}>
-            <VideoPlayer url={element.tutorialVideo} playing loop muted width="100%" height="100%" />
             <MarkdownPreviewer source={element.description} style={{ fontSize: '0.8rem', backgroundColor: '#6d6d6d', color: 'white' }} />
           </Box>
         }
